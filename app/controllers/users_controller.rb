480bfcf5e0dc9def073d581cd54ba549406d2270
Stuart Garvagh <sgarvagh@gmail.com>
2023-03-01 18:02:17 -0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,16 +13,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def login
-    user = User.where(username: params[:username]).first
-
-    if user && BCrypt::Password.new(user.password) == params[:password]
-      render nothing: true
-    else
-      render json: { error: 'Invalid username or password' }, status: 401
-    end
-  end
-
   protected
 
   def user_params
